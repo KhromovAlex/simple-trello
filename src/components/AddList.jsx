2e7a1410b14da058dd1c7451881a2231cecd1965
Alexandr Khromov <4-09@bk.ru>
2020-01-18 22:29:55 +0700

    add AddList component

---
+++ src/components/AddList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from './Button';
+import InputByPress from './InputByPress';
+import PropTypes from 'prop-types';
+import './AddList.scss';
+
+export default class AddList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isInput: false,
+        };
+    }
+
+    static propTypes = {
+        handleNewList: PropTypes.func,
+    }
+
+    toogleInput = () => {
+        this.setState((oldState) => ({
+            isInput: !oldState.isInput,
+        }));
+    }
+
+    render() {
+        const { isInput } = this.state;
+        const { handleNewList } = this.props;
+
+        return (
+            isInput ?
+            <div className="add-list-component">
+                <Button className="button_round" onClick={this.toogleInput}>X</Button>
+                <InputByPress handleInput={handleNewList} />
+            </div>
+            : <Button className="button_square" onClick={this.toogleInput}>Add a list</Button>
+        );
+    }
+}
